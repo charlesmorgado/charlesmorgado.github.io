e52b229155e32cce010c035cd3b53ef6833fd9ac
charlesmorgado.github.io <charles.w.morgado@gmail.com>
2021-05-27 00:55:47 -0500

    many changes

--- js/update.js
+++ js/update.js
@@ -1,14 +1,20 @@
-function retornarFecha(){
-    var fecha;
-    fecha=new Date();
-    let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-    let month = monthNames[fecha.getMonth()];
-    var cadena = "Last Update "+fecha.getDate()+' '+ month/*(fecha.getMonth()+1)*/+' '+fecha.getFullYear();
-    return cadena;
-  }
-  function retornarUpdate(){
-      var fecha;
-      fecha = new Date();
-      return  fecha.getFullYear();
-  
-  }
+// document.querySelector('#lastU').textContent= document.lastModified;
+document.getElementById("copyright").innerHTML = (new Date().getFullYear());
+
+
+// const hambutton= document.querySelector('.ham');
+// const mainnav = document.querySelector('.navigation');
+
+// hambutton = addEventListener('click', () =>
+// {mainnav.classList.toggle('responsive')}, false);
+
+// window.onresize = () =>{if (window.innerWidth > 760)
+// mainnav.classList.remove('responsive')};
+
+
+// const datefield = document.querySelector("date");
+
+// const now = new Date();
+// const fulldate = new Intl.DateTimeFormat("en-US",{ datestyle: "full" }).format(now);
+
+// datefield.textContent = fulldate;
